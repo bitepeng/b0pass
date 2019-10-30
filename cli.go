ccfa79b3d7a9972a46a068e2aedfe6425f9ef745
bitepeng <bitepeng@qq.com>
2019-10-30 16:52:08 +0800

    add cli args port(-p=3000)

--- cli.go
+++ cli.go
@@ -3,17 +3,29 @@ package main
 import (
 	"b0pass/boot"
 	_ "b0pass/boot"
+	"b0pass/library/ipaddress"
+	"b0pass/library/openurl"
 	_ "b0pass/router"
 	"fmt"
 	"github.com/gogf/gf/frame/g"
+	"strconv"
+	"time"
 )
 
 func main() {
 	/*
 		Wait Server
 	*/
+	//Cli Args
 	boot.ExecArgs()
+	ipArr,_:=ipaddress.GetIP()
 	fmt.Printf("[ServerUrl] http://127.0.0.1:%d\n",boot.ServPort)
+	fmt.Printf("[IPlistArr] %v\n",ipArr)
 	fmt.Printf("[Work-Path] %s\n",boot.PathRoot)
+	//Open Urls
+	go func() {
+		time.Sleep(4000 * time.Millisecond)
+		_ = openurl.Open("http://127.0.0.1:" + strconv.Itoa(boot.ServPort))
+	}()
 	g.Wait()
 }
