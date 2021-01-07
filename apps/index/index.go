373f8d8a952c58f5f6a6ff4b9af8765c3634556a
cwp <liujun@mail.com>
2021-01-07 19:29:12 +0800

    添加配置文件自定义目录

--- apps/index/index.go
+++ apps/index/index.go
@@ -2,11 +2,17 @@ package index
 
 import (
 	"b0pass/boot"
+	conf2 "b0pass/library/conf"
 	"b0pass/library/fileinfos"
 	"b0pass/library/ipaddress"
 	"github.com/gogf/gf/frame/gmvc"
+	"github.com/gogf/gf/os/gfile"
 	"strconv"
 	"time"
+)
+
+var (
+	filePath string
 )
 
 type Controller struct {
@@ -14,7 +20,7 @@ type Controller struct {
 }
 
 func (c *Controller) Index() {
-	c.View.Assign("times",time.Now().Unix())
+	c.View.Assign("times", time.Now().Unix())
 	_ = c.View.Display("index.html")
 }
 
@@ -26,21 +32,28 @@ func (c *Controller) FileLists() {
 	for _, pp := range ip {
 		ips = append(ips, pp+":"+strconv.Itoa(port))
 	}
-	c.View.Assign("ips",ips)
+	c.View.Assign("ips", ips)
 	// path
-	pathRoot := fileinfos.GetRootPath() + "/files/"
-	c.View.Assign("path_root", pathRoot)
-	// file lists
-	fprPath:=c.Request.GetString("path")
-	var fpPath string
-	if fprPath!="" {
-		fpPath="/files"+fprPath+"/*"
-	}else{
-		fpPath="/files/*"
+	//	pathRoot := fileinfos.GetRootPath() + "/files/"
+	//pathRoot := "D:/files/"
+	if gfile.Exists(fileinfos.GetRootPath() + "/config.conf") {
+		conf := conf2.InitConfig(fileinfos.GetRootPath() + "/config.conf")
+		filePath = conf["filePath"]
+	} else {
+		filePath = fileinfos.GetRootPath() + "/files/"
 	}
-	fp := fileinfos.GetRootPath() + fpPath
-	flists := fileinfos.ListDirData(fp,fprPath)
-	c.View.Assign("flists",flists)
+	c.View.Assign("path_root", filePath)
+	// file lists
+	fprPath := c.Request.GetString("path")
+	var fpPath string
+	if fprPath != "" {
+		fpPath = fprPath + "/*"
+	} else {
+		fpPath = "/*"
+	}
+	fp := filePath + fpPath
+	flists := fileinfos.ListDirData(fp, fprPath)
+	c.View.Assign("flists", flists)
 	// views
 	_ = c.View.Display("file-lists.html")
 }
